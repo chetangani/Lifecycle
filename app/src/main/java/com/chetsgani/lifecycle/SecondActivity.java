68fef875099f2efce5c12bdbfe7ad185e6ed6bd8
Chetan <cgani87@gmail.com>
2016-10-05 01:10:31 +0530

    Log Correction

--- app/src/main/java/com/chetsgani/lifecycle/SecondActivity.java
+++ app/src/main/java/com/chetsgani/lifecycle/SecondActivity.java
@@ -5,57 +5,56 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class SecondActivity extends AppCompatActivity {
+    MainActivity main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        LogStatus("onCreate", "2");
-    }
+        main = new MainActivity();
 
-    public void LogStatus(String msg, String src) {
-        Log.d("debug", msg+" Activity "+src);
+        main.LogStatus("onCreate", "2");
     }
 
     @Override
     protected void onStart() {
-        LogStatus("onStart", "2");
+        main.LogStatus("onStart", "2");
         super.onStart();
     }
 
     @Override
     protected void onResume() {
-        LogStatus("onResume", "2");
+        main.LogStatus("onResume", "2");
         super.onResume();
     }
 
     @Override
     protected void onPause() {
-        LogStatus("onPause", "2");
+        main.LogStatus("onPause", "2");
         super.onPause();
     }
 
     @Override
     protected void onStop() {
-        LogStatus("onStop", "2");
+        main.LogStatus("onStop", "2");
         super.onStop();
     }
 
     @Override
     protected void onDestroy() {
-        LogStatus("onDestroy", "2");
+        main.LogStatus("onDestroy", "2");
         super.onDestroy();
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-        LogStatus("onSaveInstanceState", "2");
+        main.LogStatus("onSaveInstanceState", "2");
         super.onSaveInstanceState(outState);
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        LogStatus("onRestoreInstanceState", "2");
+        main.LogStatus("onRestoreInstanceState", "2");
         super.onRestoreInstanceState(savedInstanceState);
     }
 }
